4e9e00c71cffc75212349ce51397d972dd947a56
tingbinhuang2019 <thuang18@usfca.edu>
2019-05-12 22:41:20 -0700

    Refactor insert query

--- firstProgram/db.py
+++ firstProgram/db.py
@@ -2,27 +2,28 @@ import mysql.connector
 from mysql.connector import errorcode
 
 DATABASE = "gold_and_silver_price"
-
 table = {}
 
 table["gold"] = (
 	"CREATE TABLE IF NOT EXISTS `gold` ("
 	"`id` int NOT NULL AUTO_INCREMENT,"
-	"`date` int NOT NULL,"
+	"`numberOfDate` int NOT NULL,"
 	"`price` decimal(6,2) NOT NULL,"
 	"`realDate` varchar(20) NOT NULL,"
+	"UNIQUE(`numberOfDate`),"
 	"PRIMARY KEY(`id`)"
-	") ENGINE=InnoDB"
+	")"
 	)
 
 table["silver"] = (
 	"CREATE TABLE IF NOT EXISTS `silver` ("
 	"`id` int NOT NULL AUTO_INCREMENT,"
-	"`date` int NOT NULL,"
+	"`numberOfDate` int NOT NULL,"
 	"`price` decimal(6,2) NOT NULL,"
 	"`realDate` varchar(20) NOT NULL,"
+	"UNIQUE(`numberOfDate`),"
 	"PRIMARY KEY(`id`)"
-	") ENGINE=InnoDB"
+	")"
 	)
 
 def passSchema():
@@ -41,6 +42,22 @@ def passSchema():
 			cur.execute(table_description)
 	cur.close()
 	cnx.close()
+
+def insertData(list, table):
+	connection = mysql.connector.connect(host='localhost', database='gold_and_silver_price', user='root', passwd='123')
+	cursor = connection.cursor(prepared=True)
+	for t in list[::-1]:
+		if table == 'gold':
+			query = """INSERT INTO gold(`numberOfDate`,`price`,`realDate`) VALUES(%s,%s,%s) ON DUPLICATE KEY UPDATE price = %s"""
+		else:
+			query = """INSERT INTO silver(`numberOfDate`,`price`,`realDate`) VALUES(%s,%s,%s) ON DUPLICATE KEY UPDATE price = %s"""
+
+		param = (t[0],t[1],t[2],t[1])
+		result = cursor.execute(query,param)
+
+		connection.commit()
+	connection.close()
+
 
 # Call function 'passSchema' to create database and tables.
 # passSchema()
